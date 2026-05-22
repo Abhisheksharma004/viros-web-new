b558b7772c3defae7952c3e5388fcf9e1e925b88
Abhishheksharma004 <abhishekkumarranjan965@gmail.com>
2026-05-22 18:08:02 +0530

    add buttom navbar in responsive

--- src/app/employee-dashboard/layout.tsx
+++ src/app/employee-dashboard/layout.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import EmployeeSidebar from "@/components/employee-dashboard/Sidebar";
 import EmployeeHeader from "@/components/employee-dashboard/Header";
+import EmployeeBottomNav from "@/components/employee-dashboard/BottomNav";
 
 export default function EmployeeDashboardLayout({
     children,
@@ -16,9 +17,10 @@ export default function EmployeeDashboardLayout({
             <EmployeeSidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
             <div className="flex-1 flex flex-col overflow-hidden">
                 <EmployeeHeader onMenuClick={() => setSidebarOpen(true)} />
-                <main className="flex-1 overflow-y-auto p-3 sm:p-6 lg:p-8">
+                <main className="flex-1 overflow-y-auto p-3 pb-20 sm:p-6 sm:pb-20 lg:p-8 lg:pb-8">
                     {children}
                 </main>
+                <EmployeeBottomNav onMenuClick={() => setSidebarOpen(true)} />
             </div>
         </div>
     );
